d19d2d34b33cefc59fc96f823eaa5912b83684b9
Wadie Benabdouh <wadie.trade@gmail.com>
2023-04-30 01:56:49 +0100

    mini documentation added in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//>> BURGER MENU SETTINGS
 const menu_btn = document.querySelector("#menu-toggle");
 const mobile_menu = document.querySelector(".mobile-nav");
 
@@ -6,7 +7,7 @@ menu_btn.onclick = () => {
   mobile_menu.classList.toggle("in-action");
 };
 
-// DARKEN BACKGROUND WHEN TOGGLING BURGER
+//>> DARKEN BACKGROUND WHEN TOGGLING BURGER
 const overlayOne = document.querySelector("body");
 
 menu_btn.addEventListener("click", function () {
@@ -14,19 +15,21 @@ menu_btn.addEventListener("click", function () {
   overlayOne.classList.toggle("active");
 });
 
-//MOBILE NAV DROPDOWN UTILITY
+//>> MOBILE NAV DROPDOWN UTILITY
+const navLinks = document.querySelectorAll(".mobile-top-link");
+const navigations = document.querySelectorAll(".mobile-dropdown");
 
-const navLinks = document.querySelectorAll('.mobile-top-link');
-const navigations = document.querySelectorAll('.mobile-dropdown');
-
-navLinks.forEach(function(link) {
-  link.addEventListener('click', function(e) {
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
     const navigations = this.nextElementSibling;
-    if (navigations.style.display === 'none' || navigations.style.display === '') {
-      navigations.style.display = 'flex';
+    if (
+      navigations.style.display === "none" ||
+      navigations.style.display === ""
+    ) {
+      navigations.style.display = "flex";
     } else {
-      navigations.style.display = 'none';
+      navigations.style.display = "none";
     }
   });
 });
